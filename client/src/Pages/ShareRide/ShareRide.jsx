6bf686f9e56bcf7941dfb0d7ce55db397378c023
tabrid <mollik15-5096@diu.edu.bd>
2024-05-13 01:38:26 +0600

    api

--- client/src/Pages/ShareRide/ShareRide.jsx
+++ client/src/Pages/ShareRide/ShareRide.jsx
@@ -151,7 +151,7 @@ function ShareRide() {
     }
     console.log(formData);
     try {
-      const response = await fetch('http://localhost:5000/api/findRoute', {
+      const response = await fetch('https://diu-rs.onrender.com/api/findRoute', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'

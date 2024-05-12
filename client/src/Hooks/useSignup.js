a0f86152f2ada9a0190a1e2ef7ca4602509699a1
tabrid <mollik15-5096@diu.edu.bd>
2024-05-12 15:23:52 +0600

    signup

--- client/src/Hooks/useSignup.js
+++ client/src/Hooks/useSignup.js
@@ -8,8 +8,8 @@ const useSignup = () => {
     const { setAuthUser } = useAuthContext();
     const navigate = useNavigate();
 
-    const signup = async ({ firstName, lastName, username, email, password, confirmPassword }) => {
-        const success = handleInputErrors({ firstName, lastName, username, email, password, confirmPassword });
+    const signup = async ({ fullName, username, email, password, confirmPassword, role, image }) => {
+        const success = handleInputErrors({ fullName, username, email, password, confirmPassword, role, imaged });
         if (!success) return;
 
         setLoading(true);
@@ -17,7 +17,7 @@ const useSignup = () => {
             const res = await fetch("/api/auth/signup", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
-                body: JSON.stringify({ firstName, lastName, username, email, password, confirmPassword }),
+                body: JSON.stringify({ fullName, username, email, password, confirmPassword, role, image }),
             });
             const data = await res.json();
             if (data.error) {
@@ -38,8 +38,8 @@ const useSignup = () => {
 
 export default useSignup;
 
-function handleInputErrors({ firstName, lastName, username, email, password, confirmPassword }) {
-    if (!firstName || !lastName || !username || !email || !password || !confirmPassword) {
+function handleInputErrors({ fullName, username, email, password, confirmPassword, role, image }) {
+    if (!fullName ||  !username || !email || !password || !confirmPassword || !role || !image) {
         toast.error("Please fill in all fields");
         return false;
     }
